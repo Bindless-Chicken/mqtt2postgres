88f6ff52ac3726f4b1e4afb6af4dfd35f345460b
Thomas Poulet <1039134+Bindless-Chicken@users.noreply.github.com>
2024-10-19 18:31:41 +0200

    Add automatic database setup

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
+use std::str::FromStr;
 use std::time::Duration;
 use chrono::Utc;
 use rumqttc::v5::{AsyncClient, Event, Incoming, MqttOptions};
 use rumqttc::v5::mqttbytes::QoS;
 use serde_json::Value;
-use tracing::{debug, error, info};
+use tracing::{error, info, warn};
 use anyhow::Result;
 use clap::Parser;
 use serde::Deserialize;
-use sqlx::{query, PgPool};
+use sqlx::{query, ConnectOptions, PgPool};
+use sqlx::any::AnyConnectOptions;
+use sqlx::migrate::MigrateDatabase;
 
 type MqttTopics = HashMap<String, Vec<String>>;
 
 #[derive(Debug, Deserialize)]
 struct FileConfig {
-    redis: PostgresConfig,
+    postgres: PostgresConfig,
     mqtt: MqttConfig,
     topics: MqttTopics,
 }
@@ -24,6 +27,7 @@ struct FileConfig {
 #[derive(Debug, Deserialize)]
 struct PostgresConfig {
     url: String,
+    setup_database: bool,
 }
 
 #[derive(Debug, Deserialize)]
@@ -51,6 +55,34 @@ fn from_file(file_path: &Path) -> Result<FileConfig> {
     Ok(toml::from_str::<FileConfig>(&config)?)
 }
 
+async fn get_database_connection_pool(config: &PostgresConfig) -> Result<PgPool> {
+    sqlx::any::install_default_drivers();
+
+    let connection_option: AnyConnectOptions = AnyConnectOptions::from_str(&config.url).unwrap();
+    if config.setup_database {
+        warn!("Setting up the database.");
+        setup_database(&connection_option).await.unwrap();
+    }
+
+
+    Ok(PgPool::connect(&config.url).await?)
+}
+
+async fn setup_database<C: ConnectOptions>(options: &C) -> Result<()> where <C as ConnectOptions>::Connection: Sized
+{
+    let url= options.to_url_lossy();
+
+    if !sqlx::Any::database_exists(url.as_str()).await? {
+        warn!("Missing database, creating it.");
+        sqlx::Any::create_database(url.as_str()).await?;
+    }
+
+    let pool = PgPool::connect(url.as_str()).await?;
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
@@ -60,7 +92,7 @@ async fn main() {
     let args = Args::parse();
     let config = from_file(&Path::new(args.config.as_str())).unwrap();
 
-    let pool = PgPool::connect(&config.redis.url).await.unwrap();
+    let pool = get_database_connection_pool(&config.postgres).await.unwrap();
 
     let mut mqtt_options = MqttOptions::new(config.mqtt.client_id, config.mqtt.host, config.mqtt.port);
     mqtt_options.set_keep_alive(Duration::from_secs(5));
@@ -110,7 +142,7 @@ async fn main() {
 }
 
 async fn add_value(pool: &PgPool, topic: &str, value: f64) -> Result<()> {
-    let rec = query!(
+    query!(
         r#"
         INSERT INTO values (time, topic, value)
         VALUES (
